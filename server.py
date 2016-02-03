e78270a17a1f5156e82481ca938f42abfdd08b40
Daniel R <rowed4@students.wwu.edu>
2016-02-02 23:11:45 -0800

    Now aborts /api GET requests with 404 error

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ from flask_restful import reqparse, Api, Resource
 from libs import convert
 
 app = Flask(__name__)
+
+##################################
+#             API                #
+##################################
+
 api = Api(app)
 
 parser = reqparse.RequestParser()
@@ -10,10 +15,7 @@ parser.add_argument('key')
 parser.add_argument('notenames')
 parser.add_argument('numerals')
 
-
 class VLSense(Resource):
-	def get(self):
-		pass
 
 	def put(self):
 		request = parser.parse_args()
@@ -23,6 +25,14 @@ class VLSense(Resource):
 
 api.add_resource(VLSense, '/api')
 
+@app.route('/api', methods=['GET'])
+def hideapi():
+	abort(404)
+	pass
+
+##################################
+#          Web Server            #
+##################################
 
 @app.route('/')
 def mainpage():
