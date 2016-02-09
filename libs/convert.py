facd76799164ca710a08aaa62313b3c163becd68
Daniel R <rowed4@students.wwu.edu>
2016-02-08 23:36:00 -0800

    small bugfixes

--- libs/convert.py
+++ libs/convert.py
@@ -1,14 +1,14 @@
 from music21 import pitch, chord, stream, key, roman
+from libs import *
 
 def convert(json):
 	skey = json['key']
-	dkey = key.Key(skey)
+	dkey = key.Key(skey.replace('b', '-'))
 	numerals = json['numerals']
 	dchords = getChords(json)
 
 
 	return {
-    	# 'title': title,
         'key': dkey,
         'skey': skey,
         # 'chords': chords,
@@ -25,19 +25,23 @@ def convert(json):
     }
 
 
-
 def getChords(json):
  	chords = []
 	rawChords = json['notenames']
 
-	for rawChord in rawChords:
-		chordList = []
-		for rawNote in rawChord:
+	# Check to see that there is at least a note
+	if (rawChords != [[u'Ab-1']]):
 
-			mnote = pitch.Pitch(rawNote)
-			chordList.append(mnote)
-			
+		for rawChord in rawChords:
+			chordList = []
+			for rawNote in rawChord:
+				rawNote = rawNote.replace('b', '-')
+				mnote = pitch.Pitch(rawNote)
+				chordList.append(mnote)
+				
+			chords.append(chord.Chord(chordList))
 
-		chords.append(chord.Chord(chordList))
+		return chords
 
-	return chords
+	else:
+		return
