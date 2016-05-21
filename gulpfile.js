a1d85b9ace2c2192d2610d2c4ff985226ca63d1a
Daniel R <rowed4@students.wwu.edu>
2016-05-21 14:17:20 -0700

    gulpfile.js runs flask

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+var exec = require('child_process').exec;
+
+gulp.task('runserver', function() {
+    var proc = exec('python app.py');
+});
+
+gulp.task('default', ['runserver'] ,function() {
   // place code for your default task here
 });
 
