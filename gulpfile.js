33f79ea95cdf157695d6bb2518999fdd03e3e848
Daniel Rowe <rowed4@students.wwu.edu>
2016-05-22 16:30:43 -0700

    gulp now handles sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,18 @@ gulp.task('runserver', function() {
     var proc = exec('python app.py');
 });
 
-gulp.task('default', ['runserver'] ,function() {
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+  return gulp.src('./static/sass/*.scss')
+      .pipe(sass({ errLogToConsole: true }))
+      .pipe(gulp.dest('./static/build/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./static/sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['runserver', 'watch', 'sass'] ,function() {
   // place code for your default task here
 });
 
